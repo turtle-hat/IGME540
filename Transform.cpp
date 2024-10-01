644c66d1444bd93fbca373afcf839a9823e3f5fe
turtle-hat <org7993@rit.edu>
2024-10-01 16:20:59 -0400

    A06: Added comments and new Camera members + functions to Transform

--- Transform.cpp
+++ Transform.cpp
@@ -8,26 +8,51 @@ using namespace DirectX;
 Transform::Transform() :
 	position(0.0f, 0.0f, 0.0f),
 	rotation(0.0f, 0.0f, 0.0f),
-	scale(1.0f, 1.0f, 1.0f)
+	scale(1.0f, 1.0f, 1.0f),
+	right(1.0f, 0.0f, 0.0f),
+	up(0.0f, 1.0f, 0.0f),
+	forward(0.0f, 0.0f, 1.0f)
 {
 	XMStoreFloat4x4(&world, XMMatrixIdentity());
+	XMStoreFloat4x4(&worldInverseTranspose,
+		XMMatrixInverse(0, XMMatrixTranspose(XMMatrixIdentity()))
+	);
+	areMatricesDirty = false;
+	areVerticesDirty = false;
 }
 
+/// <summary>
+/// Gets the Transform's position
+/// </summary>
+/// <returns>The Transform's x, y, and z positions</returns>
 DirectX::XMFLOAT3 Transform::GetPosition()
 {
 	return position;
 }
 
+/// <summary>
+/// Gets the Transform's rotation
+/// </summary>
+/// <returns>The Transform's rotations about the X, Y, and Z axes</returns>
 DirectX::XMFLOAT3 Transform::GetRotation()
 {
 	return rotation;
 }
 
+/// <summary>
+/// Gets the Transform's scale
+/// </summary>
+/// <returns>The Transform's scale along the x, y, and z axes</returns>
 DirectX::XMFLOAT3 Transform::GetScale()
 {
 	return scale;
 }
 
+/// <summary>
+/// Gets the Transform's world matrix.
+/// Rebuilds matrices if they have been mutated
+/// </summary>
+/// <returns>The world matrix representing the Transform</returns>
 DirectX::XMFLOAT4X4 Transform::GetWorld()
 {
 	if (areMatricesDirty) {
@@ -36,6 +61,11 @@ DirectX::XMFLOAT4X4 Transform::GetWorld()
 	return world;
 }
 
+/// <summary>
+/// Sets the Transform's world inverse transpose matrix.
+/// Rebuilds matrices if they have been mutated
+/// </summary>
+/// <returns>The transposed then inverted world matrix representing the Transform</returns>
 DirectX::XMFLOAT4X4 Transform::GetWorldInverseTranspose()
 {
 	if (areMatricesDirty) {
@@ -44,42 +74,99 @@ DirectX::XMFLOAT4X4 Transform::GetWorldInverseTranspose()
 	return worldInverseTranspose;
 }
 
+/// <summary>
+/// Gets the Transform's forward vector
+/// </summary>
+/// <returns></returns>
+DirectX::XMFLOAT3 Transform::GetForward()
+{
+	return forward;
+}
+
+DirectX::XMFLOAT3 Transform::GetRight()
+{
+	return right;
+}
+
+DirectX::XMFLOAT3 Transform::GetUp()
+{
+	return up;
+}
+
+/// <summary>
+/// Sets the Transform's position
+/// </summary>
+/// <param name="x">The Transform's new X position</param>
+/// <param name="y">The Transform's new Y position</param>
+/// <param name="z">The Transform's new Z position</param>
 void Transform::SetPosition(float x, float y, float z)
 {
 	position = XMFLOAT3(x, y, z);
 	areMatricesDirty = true;
 }
 
+/// <summary>
+/// Sets the Transform's position
+/// </summary>
+/// <param name="xyz">The Transform's new X, Y, and Z positions</param>
 void Transform::SetPosition(DirectX::XMFLOAT3 xyz)
 {
 	position = xyz;
 	areMatricesDirty = true;
 }
 
+/// <summary>
+/// Sets the Transform's rotation
+/// </summary>
+/// <param name="pitch">The Transform's new rotation about the X axis</param>
+/// <param name="yaw">The Transform's new rotation about the Y axis</param>
+/// <param name="roll">The Transform's new rotation about the Z axis</param>
 void Transform::SetRotation(float pitch, float yaw, float roll)
 {
 	rotation = XMFLOAT3(pitch, yaw, roll);
 	areMatricesDirty = true;
+	areVerticesDirty = true;
 }
 
+/// <summary>
+/// Sets the Transform's rotation
+/// </summary>
+/// <param name="pitchYawRoll">The Transform's new rotation about the X, Y, and Z axes</param>
 void Transform::SetRotation(DirectX::XMFLOAT3 pitchYawRoll)
 {
 	rotation = pitchYawRoll;
 	areMatricesDirty = true;
+	areVerticesDirty = true;
 }
 
+/// <summary>
+/// Sets the Transform's scale
+/// </summary>
+/// <param name="x">The Transform's new scale about the X axis</param>
+/// <param name="y">The Transform's new scale about the Y axis</param>
+/// <param name="z">The Transform's new scale about the Z axis</param>
 void Transform::SetScale(float x, float y, float z)
 {
 	scale = XMFLOAT3(x, y, z);
 	areMatricesDirty = true;
 }
 
+/// <summary>
+/// Sets the Transform's scale
+/// </summary>
+/// <param name="xyz">The Transform's new scale about the X, Y, and Z axes</param>
 void Transform::SetScale(DirectX::XMFLOAT3 xyz)
 {
 	scale = xyz;
 	areMatricesDirty = true;
 }
 
+/// <summary>
+/// Applies a translation to the Transform relative to the world
+/// </summary>
+/// <param name="x">How far to translate the Transform along the world's X axis</param>
+/// <param name="y">How far to translate the Transform along the world's Y axis</param>
+/// <param name="z">How far to translate the Transform along the world's Z axis</param>
 void Transform::MoveAbsolute(float x, float y, float z)
 {
 	// Build translation vector and pass to overload
@@ -87,6 +174,10 @@ void Transform::MoveAbsolute(float x, float y, float z)
 	MoveAbsolute(translation);
 }
 
+/// <summary>
+/// Applies a translation to the Transform relative to the world
+/// </summary>
+/// <param name="xyz">How far to translate the Transform along the X, Y, and Z axes</param>
 void Transform::MoveAbsolute(DirectX::XMFLOAT3 xyz)
 {
 	// Add translation vector to position and store the result back
@@ -94,6 +185,41 @@ void Transform::MoveAbsolute(DirectX::XMFLOAT3 xyz)
 	areMatricesDirty = true;
 }
 
+/// <summary>
+/// Applies a translation to the Transform relative to itself
+/// </summary>
+/// <param name="x">How far to translate the Transform along its X axis</param>
+/// <param name="y">How far to translate the Transform along its Y axis</param>
+/// <param name="z">How far to translate the Transform along its Z axis</param>
+void Transform::MoveRelative(float x, float y, float z)
+{
+	XMFLOAT3 translation(x, y, z);
+	MoveRelative(translation);
+}
+
+/// <summary>
+/// Applies a translation to the Transform relative to itself
+/// </summary>
+/// <param name="xyz">How far to translate the Transform along its X, Y, and Z axes</param>
+void Transform::MoveRelative(DirectX::XMFLOAT3 xyz)
+{
+	// Rotates the movement vector by the transform's rotation before adding it to the position
+	XMStoreFloat3(
+		&position, 
+		XMLoadFloat3(&position) + XMVector3Rotate(
+			XMLoadFloat3(&xyz),
+			XMQuaternionRotationRollPitchYaw(rotation.x, rotation.y, rotation.z)
+		)
+	);
+	areMatricesDirty = true;
+}
+
+/// <summary>
+/// Adds an amount to the Transform's current rotation along each world axis
+/// </summary>
+/// <param name="pitch">How much to add to the Transform's rotation about the world's X axis</param>
+/// <param name="yaw">How much to add to the Transform's rotation about the world's Y axis</param>
+/// <param name="roll">How much to add to the Transform's rotation about the world's Z axis</param>
 void Transform::Rotate(float pitch, float yaw, float roll)
 {
 	// Build rotation vector and pass to overload
@@ -101,14 +227,25 @@ void Transform::Rotate(float pitch, float yaw, float roll)
 	Rotate(rotation);
 }
 
+/// <summary>
+/// Adds an amount to the Transform's current rotation along each world axis
+/// </summary>
+/// <param name="pitchYawRoll">How much to add to the Transform's rotation about the world's X, Y, and Z axes</param>
 void Transform::Rotate(DirectX::XMFLOAT3 pitchYawRoll)
 {
 	// Add rotation vector to rotation and store the result back
 	// (I'm not worrying about gimbal lock)
 	XMStoreFloat3(&rotation, XMLoadFloat3(&rotation) + XMLoadFloat3(&pitchYawRoll));
 	areMatricesDirty = true;
+	areVerticesDirty = true;
 }
 
+/// <summary>
+/// Multiplies the Transform's scale by an amount along each of its axes
+/// </summary>
+/// <param name="x">How much to multiply to the Transform's scale along its X axis</param>
+/// <param name="y">How much to multiply to the Transform's scale along its Y axis</param>
+/// <param name="z">How much to multiply to the Transform's scale along its Z axis</param>
 void Transform::Scale(float x, float y, float z)
 {
 	// Because DirectXMath doesn't seem to include element-wise multiplication,
@@ -121,6 +258,10 @@ void Transform::Scale(float x, float y, float z)
 	areMatricesDirty = true;
 }
 
+/// <summary>
+/// Multiplies the Transform's scale by an amount along each of its axes
+/// </summary>
+/// <param name="xyz">How much to multiply to the Transform's scale along its X, Y, and Z axes</param>
 void Transform::Scale(DirectX::XMFLOAT3 xyz)
 {
 	// Probably more performant to repeat code here
@@ -133,6 +274,10 @@ void Transform::Scale(DirectX::XMFLOAT3 xyz)
 	areMatricesDirty = true;
 }
 
+/// <summary>
+/// Recalculates the Transform's World and World Inverse Transpose matrices,
+/// then marks them as no longer dirty
+/// </summary>
 void Transform::RebuildMatrices()
 {
 	// Multiply Scale to Rotation to Translation to calculate the new World matrix
@@ -151,4 +296,22 @@ void Transform::RebuildMatrices()
 
 	// Mark matrices as clean
 	areMatricesDirty = false;
+}
+
+/// <summary>
+/// Recalculates the Transform's Forward, Right, and Up vertices,
+/// then marks them as no longer dirty
+/// </summary>
+void Transform::RebuildVertices()
+{
+	XMVECTOR rotQuat = XMQuaternionRotationRollPitchYaw(rotation.x, rotation.y, rotation.z);
+	// Set all three vertices
+	right = XMFLOAT3(1.0f, 0.0f, 0.0f);
+	up = XMFLOAT3(0.0f, 1.0f, 1.0f);
+	forward = XMFLOAT3(0.0f, 0.0f, 1.0f);
+	// Rotate each vertex by the Transform's rotation values
+	XMStoreFloat3(&right, XMVector3Rotate(XMLoadFloat3(&right), rotQuat));
+	XMStoreFloat3(&up, XMVector3Rotate(XMLoadFloat3(&up), rotQuat));
+	XMStoreFloat3(&forward, XMVector3Rotate(XMLoadFloat3(&forward), rotQuat));
+	areVerticesDirty = false;
 }
--- Transform.h
+++ Transform.h
@@ -15,6 +15,9 @@ public:
 	DirectX::XMFLOAT3 GetScale();
 	DirectX::XMFLOAT4X4 GetWorld();
 	DirectX::XMFLOAT4X4 GetWorldInverseTranspose();
+	DirectX::XMFLOAT3 GetForward();
+	DirectX::XMFLOAT3 GetRight();
+	DirectX::XMFLOAT3 GetUp();
 
 	// Setters
 	void SetPosition(float x, float y, float z);
@@ -27,6 +30,8 @@ public:
 	// Mutators
 	void MoveAbsolute(float x, float y, float z);
 	void MoveAbsolute(DirectX::XMFLOAT3 xyz);
+	void MoveRelative(float x, float y, float z);
+	void MoveRelative(DirectX::XMFLOAT3 xyz);
 	void Rotate(float pitch, float yaw, float roll);
 	void Rotate(DirectX::XMFLOAT3 pitchYawRoll);
 	void Scale(float x, float y, float z);
@@ -43,10 +48,18 @@ private:
 	DirectX::XMFLOAT4X4 world;
 	// Final Inverse Transpose World Matrix
 	DirectX::XMFLOAT4X4 worldInverseTranspose;
+	// Forward, right, and up vectors for the Transform
+	DirectX::XMFLOAT3 forward;
+	DirectX::XMFLOAT3 right;
+	DirectX::XMFLOAT3 up;
 
 	// Whether World and World Inverse Transpose matrices needs to be rebuilt
 	bool areMatricesDirty;
+	// Whether Forward, Right, and Up vertices need to be rebuilt
+	bool areVerticesDirty;
 
 	// Rebuilds World and WorldInverseTranspose
 	void RebuildMatrices();
+	// Rebuilds Forward, Right, and Up vertices
+	void RebuildVertices();
 };
